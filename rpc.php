88028869b16bac2f2d992c07b818c82bbd376001
Bankole Abiodun <110631073+meowbanky@users.noreply.github.com>
2026-01-26 22:19:57 +0000

    chore: remove social_posts from remote and update gitignore

---
+++ rpc.php
@@ -0,0 +1,61 @@
+﻿<?php
+	
+	// PHP5 Implementation - uses MySQLi.
+    if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+        require_once __DIR__ . '/vendor/autoload.php';
+    }
+    if (class_exists('Dotenv\Dotenv')) {
+        $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+        $dotenv->safeLoad();
+    }
+
+    $db_host = $_ENV['DB_HOST'];
+    $db_user = $_ENV['DB_USERNAME'];
+    $db_pass = $_ENV['DB_PASSWORD'];
+    $db_name = $_ENV['DB_NAME'];
+
+	// mysqli('localhost', 'yourUsername', 'yourPassword', 'yourDatabase');
+$db = new mysqli($db_host, $db_user, $db_pass, $db_name);
+	
+	if(!$db) {
+		// Show error if we cannot connect.
+		echo 'ERROR: Could not connect to the database.';
+	} else {
+		// Is there a posted query string?
+		if(isset($_POST['queryString'])) {
+			$queryString = $db->real_escape_string($_POST['queryString']);
+			
+			// Is the string length greater than 0?
+			
+			if(strlen($queryString) >0) {
+				// Run the query: We use LIKE '$queryString%'
+				// The percentage sign is a wild-card, in my example of countries it works like this...
+				// $queryString = 'Uni';
+				// Returned data = 'United States, United Kindom';
+				
+				// YOU NEED TO ALTER THE QUERY TO MATCH YOUR DATABASE.
+				// eg: SELECT yourColumnName FROM yourTable WHERE yourColumnName LIKE '$queryString%' LIMIT 10
+				
+				$query = $db->query("SELECT tbl_personalinfo.memberid, tbl_personalinfo.Fname, tbl_personalinfo.Mname, tbl_personalinfo.Lname, tbl_personalinfo.MobilePhone FROM tbl_personalinfo WHERE tbl_personalinfo.patientid LIKE '%$queryString%' OR tbl_personalinfo.Fname LIKE '%$queryString%' OR tbl_personalinfo.Mname LIKE '%$queryString%' OR tbl_personalinfo.Lname LIKE '%$queryString%' OR tbl_personalinfo.MobilePhone LIKE '%$queryString%' LIMIT 5");
+				if($query) {
+					// While there are results loop through them - fetching an Object (i like PHP5 btw!).
+					while ($result = $query ->fetch_object()) {
+						// Format the results, im using <li> for the list, you can change it.
+						// The onClick function fills the textbox with the result.
+						
+						// YOU MUST CHANGE: $result->value to $result->your_colum
+	         			echo '<li onClick="fill2(\''.$result->Lname.' '. $result->Fname . ' '. $result->Mname .'\');fill(\''.$result->memberid.'\')">'.$result->memberid. "<br>" .$result->Lname. " " . $result->Fname . " " . $result->Mname .'</li>';
+	         		
+					
+					}
+				} else {
+					echo 'ERROR: There was a problem with the query.';
+				}
+			} else {
+				// Dont do anything.
+			} // There is a queryString.
+		} else {
+			echo 'There should be no direct access to this script!';
+		}
+	}
+?>
